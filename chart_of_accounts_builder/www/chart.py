1cb5d03e9b919ad76461316f95b79e3feaf5ce0f
Chinmay D. Pai <chinmaydpai@gmail.com>
2020-05-06 13:22:50 +0530

    fix: sanitize search results before displaying
    Signed-off-by: Chinmay D. Pai <chinmaydpai@gmail.com>

--- chart_of_accounts_builder/www/chart.py
+++ chart_of_accounts_builder/www/chart.py
@@ -2,13 +2,14 @@ import frappe
 from frappe.website.utils import get_comment_list
 
 def get_context(context):
-	context.accounts = frappe.get_all("Account", filters={"company": frappe.form_dict.company},
+	company = frappe.utils.sanitize_html(frappe.form_dict_company)
+	context.accounts = frappe.get_all("Account", filters={"company": company},
 		fields=["account_name", "account_number", "name", "is_group", "parent_account", 
 			"account_type", "company", "root_type", "tax_rate"], order_by="lft asc")
 
-	context.title = frappe.form_dict.company
-	context.stars = frappe.db.get_value("Company", frappe.form_dict.company, "stars")
-	context.comment_list = get_comment_list("Company", frappe.form_dict.company)
+	context.title = company
+	context.stars = frappe.db.get_value("Company", company, "stars")
+	context.comment_list = get_comment_list("Company", company)
 	context.reference_doctype = "Company"
-	context.reference_name = frappe.form_dict.company
+	context.reference_name = company
 	context.no_cache = True
